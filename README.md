570a6c9315b846c1c3522e8170038334cf4e1d68
Nitin <nitinnb31122002@gmail.com>
2023-10-30 09:49:42 +0530

    Day-3

---
+++ README.md
@@ -0,0 +1,3 @@
+## Acknowledgements
+
+-   [Data structure and algorithms by Codevolution](https://youtube.com/playlist?list=PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP&si=cnrlMYqqPQnov19o)
---
+++ js/3SearchAlgorithms/15_LinearSearch.js
@@ -0,0 +1,16 @@
+// Linear Search
+// Given an array of 'n' elements and a target element 't', find the index of 't' in the array.
+// Return -1 if the target element is not found.
+
+// Time Complexity = O(n)
+
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) return i;
+    }
+    return -1;
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // 2
+console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // 4
+console.log(linearSearch([-5, 2, 10, 4, 6], 1)); // -1
