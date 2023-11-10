6bea31abb3c920ee7107a517ef4aa2e22acdff0c
Nitin <nitinnb31122002@gmail.com>
2023-11-10 10:30:24 +0530

    Hash start

---
+++ js/6DataStructures/8-HashMap/1_HashMap.js
@@ -0,0 +1,14 @@
+// Hash Map or Hash Table
+
+/**
+ * 1) A hash table, also known as hash map, is a ds that is used to store key-value pairs.
+ * 2) Given a key, you can associate a value with that key for very fast lookup.
+ * 3) Js's Object is a speacial implementation of the hash table ds.
+ * 4) However, Object class adds its own keys. Keys that you i/p may conflict and overwrite the inherited default properties.
+ * 5) Maps which were introduced in 2015 allow you to store key-value pairs.
+ *
+ * // Usage/ Applications
+ * - Hash tables are typically implemented where constant time lookup and insertion are required.
+ * - Database indexing.
+ * - Caches
+ */
