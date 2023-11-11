dc755eb2556d7267f276f5ac6676162443fd4425
Nitin <nitinnb31122002@gmail.com>
2023-11-11 08:41:36 +0530

    Tree

---
+++ js/6DataStructures/8-Trees/1_Tree.js
@@ -0,0 +1,47 @@
+// Tree
+/**
+ * 1) A tree is a hierarchical ds that consists of nodes connected by edges.
+ * 2) A tree is a non-linear ds, compared to arrays, etc. which are linear.
+ * 3) In linear ds, the time required to search is proportional to the size of the data set.
+ * 4) Trees however, owing to the nonlinear nature allow quicker and easier access to the data.
+ *
+ * // Tree usage
+ * - DOM
+ * - File system for directory structure
+ * - Family tree
+ *
+ *
+ * Degree of a tree is the maximum degree of the node.
+ */
+
+// Binary Tree
+/**
+ * 1) A binary tree is tree ds in which each node has at most 2 children.
+ */
+
+// Binary Search Tree
+/**
+ * - is a binary tree in which :
+ * 1) The value of each left node must be smaller than the parent node.
+ * 2) The value of each right node must be greater than the parent node.
+ */
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    isEmpty() {
+        return this.root === null;
+    }
+}
+
+const bst = new BinarySearchTree();
+console.log(bst.isEmpty());
