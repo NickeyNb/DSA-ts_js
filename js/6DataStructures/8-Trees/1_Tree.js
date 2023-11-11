1bf5755143d7b3f7cd8507634e13111412aaea86
Nitin <nitinnb31122002@gmail.com>
2023-11-11 09:01:53 +0530

    insert node

--- js/6DataStructures/8-Trees/1_Tree.js
+++ js/6DataStructures/8-Trees/1_Tree.js
@@ -41,7 +41,33 @@ class BinarySearchTree {
     isEmpty() {
         return this.root === null;
     }
+    insert(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.root = node;
+        } else {
+            this.insertNode(this.root, node);
+        }
+    }
+    insertNode(root, node) {
+        if (node.value < root.value) {
+            if (root.left === null) {
+                root.left = node;
+            } else {
+                this.insertNode(root.left, node);
+            }
+        } else {
+            if (root.right === null) {
+                root.right = node;
+            } else {
+                this.insertNode(root.right, node);
+            }
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
 console.log(bst.isEmpty());
+bst.insert(10);
+bst.insert(5);
+bst.insert(3);
